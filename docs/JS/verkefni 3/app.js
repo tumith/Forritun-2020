e9a0c57d710f6c9466e337f400553fa4622b41b7
tumith <tumithorberg@gmail.com>
2020-03-26 15:27:27 +0000

    verk 3 video 6

--- docs/JS/verkefni 3/app.js
+++ docs/JS/verkefni 3/app.js
@@ -1,6 +1,10 @@
 /*video numer 6*/
 const endpoint = 'https://apis.is/petrol';
 
+let valid_95 = true;
+let erAfslatturmedBen = false;
+let erAfslatturmedDis = false;
+
 const petrolStations = [];
 fetch(endpoint).then(blob => blob.json()).then(data => {
     console.log(data);
@@ -9,7 +13,7 @@ fetch(endpoint).then(blob => blob.json()).then(data => {
 });
 
 function findMatches(wordToMatch, petrolStations) {
-    console.log("petrolStations:",petrolStations);
+    console.log('petrolStations:',petrolStations);
     return petrolStations.filter(place => {
         // Hér er verið að ryna að finna út hvort það sem var skrifað var er eitthver staðar í json skráni
         const regex = new RegExp(wordToMatch, 'gi');
@@ -26,35 +30,76 @@ function numberWithCommas(x) {
 /*-----------------------------------------*/
 
 function displayMatches() {
-    const matchArray = findMatches(this.value, petrolStations);
-    console.log("matchArray",matchArray);
+    const matchArray = findMatches(searchInput.value, petrolStations);
+    console.log('matchArray',matchArray);
     
     while (bensinstodvar.firstChild) { bensinstodvar.removeChild(bensinstodvar.firstChild); }
+    if (matchArray.length !== 0){
+        let resoultNumber = document.createElement('li');
+        resoultNumber.textContent = 'Fjöldi leitar niðurstaða  ' + matchArray.length;
+        resoultNumber.style = 'color: blue;';
+        bensinstodvar.appendChild(resoultNumber);
+    }
 
     matchArray.forEach(bensinstod => {
-        let bensinstod_node = document.createElement("li");
+        let bensinstod_node = document.createElement('li');
         bensinstod_node.textContent = bensinstod.name + ', ' + bensinstod.company;
-        let verd_node = document.createElement("span");
+        let verd_node = document.createElement('span');
         if (valid_95 === true){
-            verd_node.textContent = bensinstod.bensin95 + "kr";
+            verd_node.textContent = bensinstod.bensin95 + 'kr';
+            if (erAfslatturmedBen === true){
+                verd_node.textContent = bensinstod.bensin95_discount + 'kr';
+                document.getElementById('leita').placeholder = 'Leita af Bensíni með afslætti';
+            }
         }else{
-            verd_node.textContent = bensinstod.diesel + "kr";
+            verd_node.textContent = bensinstod.diesel + 'kr';
+            if (erAfslatturmedDis === true){
+                verd_node.textContent = bensinstod.diesel_discount + 'kr';
+                document.getElementById('leita').placeholder = 'Leita af Dísel með afslætti';
+            }
         }
-        console.log("verð",verd_node);
+        console.log('verð',valid_95);
         bensinstod_node.appendChild(verd_node);
         bensinstodvar.appendChild(bensinstod_node);
     });
 
     if (matchArray.length === 0) {
-        let bensinstod_node = document.createElement("li");
-        bensinstod_node.textContent = "Engar niðurstöður fundust";
+        let bensinstod_node = document.createElement('li');
+        bensinstod_node.textContent = 'Engar niðurstöður fundust';
+        bensinstod_node.style = 'color: red;';
         bensinstodvar.appendChild(bensinstod_node);
     }
 }
 
 const searchInput = document.querySelector('.search');
 const bensinstodvar = document.getElementById('bensinstodvar');
-let  valid_95 = true;
+const buttonBen = document.getElementById('bensín95Id');
+const buttonDisl = document.getElementById('díselId');
+
+buttonBen.addEventListener('click', evt => {
+    valid_95 = true;
+    erAfslatturmedDis = false;
+    erAfslatturmedBen = false;
+    displayMatches();
+    document.getElementById('leita').placeholder = 'Leita af Bensíni';
+});
+buttonDisl.addEventListener('click', evt => {
+    valid_95 = false;
+    erAfslatturmedDis = false;
+    erAfslatturmedBen = false;
+    displayMatches();
+    document.getElementById('leita').placeholder = 'Leita af Dísel';
+});
+const resoult = document.getElementById('searchResoult');
+const aflatt = document.getElementById('medLykil');
+aflatt.addEventListener('click', evt => {
+    erAfslatturmedBen = true;
+    erAfslatturmedDis = true;
+    displayMatches();
+})
+
 
 searchInput.addEventListener('change', displayMatches);
 searchInput.addEventListener('keyup', displayMatches);
+
+/*Credit to Hrólfur Gylfason*/
