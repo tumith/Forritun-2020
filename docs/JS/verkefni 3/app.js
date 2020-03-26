690e27d7753a4d789df52f5e62a0e44f056342f1
tumith <tumithorberg@gmail.com>
2020-03-26 13:20:57 +0000

    verkefni 3 fékk hjálp frá hrólfi

--- docs/JS/verkefni 3/app.js
+++ docs/JS/verkefni 3/app.js
@@ -2,15 +2,18 @@
 const endpoint = 'https://apis.is/petrol';
 
 const petrolStations = [];
-fetch(endpoint)
-    .then(blob => blob.json())
-    .then(data => petrolStations.push(...data));
+fetch(endpoint).then(blob => blob.json()).then(data => {
+    console.log(data);
+    
+    petrolStations.push(...data.results)
+});
 
 function findMatches(wordToMatch, petrolStations) {
+    console.log("petrolStations:",petrolStations);
     return petrolStations.filter(place => {
         // Hér er verið að ryna að finna út hvort það sem var skrifað var er eitthver staðar í json skráni
         const regex = new RegExp(wordToMatch, 'gi');
-        return place.name.match(regex) || place.company.match(regex)
+        return place.name.match(regex) || place.company.match(regex);
     });
 }
 
@@ -24,22 +27,34 @@ function numberWithCommas(x) {
 
 function displayMatches() {
     const matchArray = findMatches(this.value, petrolStations);
-    const html = matchArray.map(place =>{
-        const regex = new RegExp(this.value, 'gi');
-        const petrolName = place.name.replace(regex, `<span class ="h1">${this.value}</span>`);
-        const petrolCompany = place.company.replace(regex, `<sapn class="h1">${this.value}</span>`);
-        return `
-            <li>
-                <span class = "name"> ${petrolName}, ${petrolCompany}</span>
-                <span class = "population">${numberWithCommas(place.diesel)}</span>
-            </li>
-        `;
-    }).join('');
-    suggestions.innerHtml = html;
+    console.log("matchArray",matchArray);
+    
+    while (bensinstodvar.firstChild) { bensinstodvar.removeChild(bensinstodvar.firstChild); }
+
+    matchArray.forEach(bensinstod => {
+        let bensinstod_node = document.createElement("li");
+        bensinstod_node.textContent = bensinstod.name + ', ' + bensinstod.company;
+        let verd_node = document.createElement("span");
+        if (valid_95 === true){
+            verd_node.textContent = bensinstod.bensin95 + "kr";
+        }else{
+            verd_node.textContent = bensinstod.diesel + "kr";
+        }
+        console.log("verð",verd_node);
+        bensinstod_node.appendChild(verd_node);
+        bensinstodvar.appendChild(bensinstod_node);
+    });
+
+    if (matchArray.length === 0) {
+        let bensinstod_node = document.createElement("li");
+        bensinstod_node.textContent = "Engar niðurstöður fundust";
+        bensinstodvar.appendChild(bensinstod_node);
+    }
 }
 
 const searchInput = document.querySelector('.search');
-const suggestions = document.querySelector('.suggestions');
+const bensinstodvar = document.getElementById('bensinstodvar');
+let  valid_95 = true;
 
 searchInput.addEventListener('change', displayMatches);
 searchInput.addEventListener('keyup', displayMatches);
