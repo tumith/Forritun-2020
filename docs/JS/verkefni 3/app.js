ef79dd4afed32d47f37f838a642cd8677ed420be
tumith <tumithorberg@gmail.com>
2020-03-27 13:28:45 +0000

    konmai code

--- docs/JS/verkefni 3/app.js
+++ docs/JS/verkefni 3/app.js
@@ -1,16 +1,30 @@
 const panels = document.querySelectorAll('.panel');
 
-    function toggleOpen() {
-      console.log('Hello');
-      this.classList.toggle('open');
-    }
+function toggleOpen() {
+    console.log('Hello');
+    this.classList.toggle('open');
+}
 
-    function toggleActive(e) {
-      console.log(e.propertyName);
-      if (e.propertyName.includes('flex')) {
-        this.classList.toggle('open-active');
-      }
+function toggleActive(e) {
+    console.log(e.propertyName);
+    if (e.propertyName.includes('flex')) {
+    this.classList.toggle('open-active');
     }
+}
 
-    panels.forEach(panel => panel.addEventListener('click', toggleOpen));
-    panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+
+const pressed = [];
+const secretCode = 'motomoto';
+
+window.addEventListener('keyup', (e) => {
+  console.log(e.key);
+  pressed.push(e.key);
+  pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+  if (pressed.join('').includes(secretCode)) {
+    console.log('DING DING!');
+    cornify_add();
+  }
+  console.log(pressed);
+});
