1f20092f22c91196776d866bb4eed58fc52dc51b
tumith <tumithorberg@gmail.com>
2020-03-27 17:46:42 +0000

    video 20

--- docs/JS/verkefni 3/video20/app.js
+++ docs/JS/verkefni 3/video20/app.js
@@ -1,4 +1,4 @@
-window.SpeechRecognition = window.SpeechRecognition.SpeechRecognition || window.webkitSpeechRecognition;
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition();
 recognition.interimResults = true;
