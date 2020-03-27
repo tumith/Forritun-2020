79f88976732acbe24a7fbf450f0ec6cb4c328045
tumith <tumithorberg@gmail.com>
2020-03-27 14:17:34 +0000

    konami code 2

--- docs/JS/verkefni 3/app.js
+++ docs/JS/verkefni 3/app.js
@@ -1,3 +1,4 @@
+/*Flexable background picure*/
 const panels = document.querySelectorAll('.panel');
 
 function toggleOpen() {
@@ -14,9 +15,12 @@ function toggleActive(e) {
 
 panels.forEach(panel => panel.addEventListener('click', toggleOpen));
 panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+/*--------------------------------------------------------- */
 
+/*Konami code*/
 const pressed = [];
 const secretCode = 'motomoto';
+const konamiCode = 'gunnar';
 
 window.addEventListener('keyup', (e) => {
   console.log(e.key);
@@ -29,7 +33,14 @@ window.addEventListener('keyup', (e) => {
     document.getElementById('newpic3').style.backgroundImage="url('https://cdn.discordapp.com/attachments/327134378780262411/693088040121270322/yahoo_2.jpg')";
     document.getElementById('newpic4').style.backgroundImage="url('https://cdn.discordapp.com/attachments/327134378780262411/693088040121270322/yahoo_2.jpg')";
     document.getElementById('newpic5').style.backgroundImage="url('https://cdn.discordapp.com/attachments/327134378780262411/693088040121270322/yahoo_2.jpg')";
+  } 
+  if (pressed.join('').includes(konamiCode)) {
+    console.log('virkni');
+    document.getElementById('newpic1').style.backgroundImage="url('https://tskoli.is/wp-content/uploads/2017/11/Gunnar-Thorunnarson-150x150.jpg')";
+    document.getElementById('newpic2').style.backgroundImage="url('https://tskoli.is/wp-content/uploads/2017/11/Gunnar-Thorunnarson-150x150.jpg')";
+    document.getElementById('newpic3').style.backgroundImage="url('https://tskoli.is/wp-content/uploads/2017/11/Gunnar-Thorunnarson-150x150.jpg')";
+    document.getElementById('newpic4').style.backgroundImage="url('https://tskoli.is/wp-content/uploads/2017/11/Gunnar-Thorunnarson-150x150.jpg')";
+    document.getElementById('newpic5').style.backgroundImage="url('https://tskoli.is/wp-content/uploads/2017/11/Gunnar-Thorunnarson-150x150.jpg')";
   }
-
   console.log(pressed);
 });
