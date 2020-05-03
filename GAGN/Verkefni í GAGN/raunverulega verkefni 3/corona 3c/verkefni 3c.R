85419553b924ff954c90c9ae26fac3dc6880c8c2
tumith <tumithorberg@gmail.com>
2020-05-03 19:48:33 +0000

    verkefni 3c

---
+++ GAGN/Verkefni í GAGN/raunverulega verkefni 3/corona 3c/verkefni 3c.R
@@ -0,0 +1,19 @@
+# covid_19_clean_complete comes from "https://www.kaggle.com/imdevskp/corona-virus-report"
+library(expss)
+
+summary(covid_19_clean_complete)
+
+cro(covid_19_clean_complete[2])
+
+cat(sprintf("country = \"%s\" , 
+            DATE = \"%s\" , 
+            Confirmed = \"%f\" , 
+            Total Deaths = \"%f\" , 
+            Recoveries = \"%f\"\n", 
+            covid_19_clean_complete$`Country/Region`, 
+            covid_19_clean_complete$Date,
+            covid_19_clean_complete$Confirmed,
+            covid_19_clean_complete$Deaths, 
+            covid_19_clean_complete$Recovered
+            )
+    )
